aa6cdb956cda5d852baed4e9348e41c8a59df1a5
crvernon <chrisrvernon@gmail.com>
2018-04-24 11:08:40 -0400

    updated to proxymod name

---
+++ proxymod/logger.py
@@ -0,0 +1,63 @@
+"""
+Logger for box model.
+
+@author:  Chris R. Vernon (chris.vernon@pnnl.gov)
+"""
+
+import datetime
+import logging
+import sys
+
+
+loggers = {}
+
+
+def make_log(out_dir, model_name):
+    """
+    Create a log file and console log handler.
+    :param:         full path to output dir where the log file is to be saved.
+    """
+
+    global loggers
+
+    nm = 'proxymod_logger'
+
+    if loggers.get(nm):
+        return loggers.get(nm)
+
+    else:
+        log = logging.getLogger(nm)
+        log.setLevel(logging.INFO)
+
+        dt = datetime.datetime.now().strftime('%Y-%m-%d_%Hh%Mm%Ss')
+
+        # file handler
+        fh = logging.FileHandler('{}/{}_{}.log'.format(out_dir, model_name, dt))
+
+        # stream handler
+        cns = logging.StreamHandler(sys.stdout)
+
+        # logger string format
+        fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # set format
+        fh.setFormatter(fmt)
+        cns.setFormatter(fmt)
+
+        log.addHandler(fh)
+        log.addHandler(cns)
+
+        loggers.update(dict(name=log))
+
+        return log
+
+
+def kill_log(log):
+    """
+    Remove any existing handlers.
+    :param:         log object
+    """
+    handlers = log.handlers[:]
+    for h in handlers:
+        h.close()
+        log.removeHandler(h)
---
+++ proxymod/model.py
@@ -0,0 +1,233 @@
+"""
+Model interface for box model.
+
+@author:  Chris R. Vernon (chris.vernon@pnnl.gov)
+"""
+
+import logger
+import math
+import time
+
+from config_reader import ReadConfig
+
+
+class Prox:
+
+    def __init__(self, config=None, model_name=None, start_yr=None, end_yr=None, step=None,
+                 target_yr=None, in_one=None, in_two=None):
+
+        self.c = ReadConfig(config, model_name, in_one, in_two)
+
+        # start year in YYYY format
+        if start_yr is None:
+            self.start_year = start_yr
+        else:
+            self.start_year = int(start_yr)
+
+        # end year in YYYY format
+        if end_yr is None:
+            self.end_year = end_yr
+        else:
+            self.end_year = int(end_yr)
+
+        # time step in years
+        if step is None:
+            self.step = step
+        else:
+            self.step = int(step)
+
+        # get target year
+        if target_yr is None:
+            self.target_yr = target_yr
+        else:
+            self.target_yr = int(target_yr)
+
+        self.yr_range = self.set_yr_range()
+
+        # set initial values for for sum and max params
+        if in_one is None:
+            self.param_1 = None
+        else:
+            self.param_1 = in_one
+
+        if in_two is None:
+            self.param_2 = None
+        else:
+            self.param_2 = in_two
+
+        self.log = self.c.log
+
+        if target_yr is None and start_yr is None:
+            msg = "Either 'target_yr' or 'start_yr, end_yr, and step' need to be defined."
+            self.log.error(msg)
+            raise(ValueError(msg))
+
+        elif target_yr is None:
+            self.run = 'all'
+        else:
+            self.run = 'step'
+
+        self.log.info('Starting {}'.format(self.c.model_name))
+
+        # mirror configuration file in log file
+        self.log_config()
+
+        self.prepare()
+
+        self.idx = 0
+        self.set_yr = 0
+
+    def prepare(self):
+        """
+        Run proxymod model.
+        """
+        # pause model before running code based on user setting
+        time.sleep(self.c.runtime)
+
+        # raise error if user wants a failure
+        if self.c.failure == 1:
+            msg = "You raised an error on purpose."
+            self.log.error(msg)
+            raise(RuntimeError(msg))
+
+        # read and modify input data
+        self.read_data()
+
+    def close(self):
+
+        self.log.info('Completed {}'.format(self.c.model_name))
+
+        # remove any handlers that may exist
+        logger.kill_log(self.log)
+
+    def set_yr_range(self):
+        """
+        Set year range to evaluate.
+
+        :return:                list of years
+        """
+
+        if (self.start_year is None) and (self.end_year is None):
+            return [self.target_yr]
+
+        else:
+            return range(self.start_year, self.end_year + self.step, self.step)
+
+    def modify(self):
+        """
+        Modify dictionary values to be square root of each value.
+
+        :param d:               dictionary {yr: value}
+        :return:                dictionary {yr: value}
+        """
+        yr = str(self.set_yr)
+        self.param_1[yr] = math.sqrt(self.param_1[yr])
+        self.param_2[yr] = math.sqrt(self.param_2[yr])
+
+    @staticmethod
+    def read_csv(f, yr_range):
+        """
+        Read CSV to dictionary in format { yr: value }
+
+        :param f:               full path with filename and extension of input CSV
+        :return:                dictionary
+        """
+        d = {}
+        with open(f, 'rU') as o:
+
+            # pass header
+            o.next()
+
+            for line in o:
+                row = line.strip().split(',')
+
+                if int(row[0]) in yr_range:
+
+                    d[row[0]] = float(row[1])
+
+        return d
+
+    def read_data(self):
+        """
+        Read inputs to dictionary and modify values.
+        """
+        typ_1 = type(self.c.in_file_one)
+        typ_2 = type(self.c.in_file_two)
+
+        # if both inputs are CSV
+        if (typ_1 is str) and (typ_2 is str):
+
+            self.param_1 = self.read_csv(self.c.in_file_one, self.yr_range)
+            self.param_2 = self.read_csv(self.c.in_file_two, self.yr_range)
+
+        elif (typ_1 is str) and (typ_2 is dict):
+
+            self.param_1 = self.read_csv(self.c.in_file_one, self.yr_range)
+            self.param_2 = self.c.in_file_two
+
+        elif (typ_1 is dict) and (typ_2 is str):
+
+            self.param_1 = self.c.in_file_one
+            self.param_2 = self.read_csv(self.c.in_file_two, self.yr_range)
+
+        elif (typ_1 is dict) and (typ_2 is dict):
+
+            self.param_1 = self.c.in_file_one
+            self.param_2 = self.c.in_file_two
+
+        else:
+            msg = "Type for either input one or two must be either 'str' or 'dict'"
+            self.log.error(msg)
+            raise(ValueError(msg))
+
+    @staticmethod
+    def build_output(f, d):
+        """
+
+        :return:
+        """
+        with open(f, 'w') as out:
+
+            out.write('year,value\n')
+
+            for k in d.keys():
+                out.write('{},{}\n'.format(k, d[k]))
+
+    def advance(self):
+        """
+        Execute time steps.  Create two output files that 1) sum values in file one
+        and 2) get the max of the values in file two.
+        """
+        if self.idx == 0:
+            self.set_yr = self.start_year
+            self.idx += 1
+        else:
+            self.set_yr += self.step
+
+        self.modify()
+
+        if self.run == 'all':
+            yrs = '{}_{}'.format(self.start_year, self.end_year)
+        else:
+            yrs = self.target_yr
+
+        of_1 = self.c.out_file_one.format(self.c.model_name, yrs)
+        of_2 = self.c.out_file_two.format(self.c.model_name, yrs)
+
+        self.build_output(of_1, self.param_1)
+        self.build_output(of_2, self.param_2)
+
+    def log_config(self):
+        """
+        Log validated configuration options.
+        """
+        for i in dir(self.c):
+
+            # create configuration object from string
+            x = eval('self.c.{0}'.format(i))
+
+            # ignore magic objects
+            if type(x) == str and i[:2] != '__':
+
+                # log result
+                self.log.info('CONFIG: [PARAMETER] {0} -- [VALUE] {1}'.format(i, x))
