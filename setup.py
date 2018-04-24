a61fefdd77c9dd200efb639a96e76e51a010f4a5
crvernon <chrisrvernon@gmail.com>
2018-04-24 11:48:09 -0400

    update requirements for setup

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from distutils.core import setup
+import sys
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    raise('Missing Package: proxymod requires setuptools to install. Please install setuptools and retry.')
+
+
+def get_requirements():
+    with open('requirements.txt') as f:
+        return f.read().split()
+
 
 setup(
-    name='box',
+    name='proxymod',
     version='v0.0.1',
-    packages=['box'],
-    url='https://github.com/IMMM-SFA/box',
+    packages=find_packages(),
+    url='https://github.com/IMMM-SFA/proxymod',
     license='GNU V2',
     author='Chris R. Vernon',
     author_email='chris.vernon@pnnl.gov',
-    description='Proxy model for testing model interactions'
+    description='A lightweight Python package to simulate model interactions',
+    install_requires=get_requirements()
 )
