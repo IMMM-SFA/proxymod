e04f634b05375559bcef186776f19122229d0b18
crvernon <chris.vernon@pnnl.gov>
2019-01-10 17:38:25 -0500

    updated setup version and license

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-import sys
-
 
 try:
     from setuptools import setup, find_packages
 except ImportError:
-    raise('Missing Package: proxymod requires setuptools to install. Please install setuptools and retry.')
+    raise('Missing Package: proxymod requires "setuptools" to install. Install "setuptools" and retry.')
 
 
 def get_requirements():
@@ -12,14 +10,20 @@ def get_requirements():
         return f.read().split()
 
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name='proxymod',
-    version='v0.0.1',
+    version='0.1.0',
     packages=find_packages(),
     url='https://github.com/IMMM-SFA/proxymod',
-    license='GNU V2',
+    license='BSD 2-Clause',
     author='Chris R. Vernon',
     author_email='chris.vernon@pnnl.gov',
     description='A lightweight Python package to simulate model interactions',
+    long_description=readme(),
     install_requires=get_requirements()
 )
