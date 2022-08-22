fd3f51454409dece32c9fb46c56d932262e9d48b
Chris Vernon <chrisrvernon@gmail.com>
2022-08-22 13:23:38 -0400

    updating install protocol

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    raise('Missing Package: proxymod requires "setuptools" to install. Install "setuptools" and retry.')
-
-
-def get_requirements():
-    with open('requirements.txt') as f:
-        return f.read().split()
+import re
+from setuptools import setup, find_packages
 
 
 def readme():
+    """Return the contents of the project README file."""
+
     with open('README.md') as f:
         return f.read()
 
 
+version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", open('proxymod/__init__.py').read(), re.M).group(1)
+
 setup(
     name='proxymod',
-    version='0.1.0',
+    version=version,
     packages=find_packages(),
     url='https://github.com/IMMM-SFA/proxymod',
-    license='BSD 2-Clause',
+    license='BSD-2-Clause',
     author='Chris R. Vernon',
     author_email='chris.vernon@pnnl.gov',
     description='A lightweight Python package to simulate model interactions',
     long_description=readme(),
-    install_requires=get_requirements()
+    long_description_content_type="text/markdown",
+    python_requires='>=3.8.*, <4',
+    include_package_data=True,
+    install_requires=[
+        "configobj>=5.0.6",
+
+    ],
+    extras_require={
+        'dev': [
+        ]
+    }
 )
